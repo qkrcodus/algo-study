72ed1fdf09b74e29952ea6d05aeb5bed6f45d059
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2023-11-08 16:49:30 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+int T, R;
+char S[21];
+
+scanf("%d", &T);
+//T만큼의 테스트케이스
+while (T--) {
+	scanf("%d %s", &R, S);
+
+	//결과를 출력하기 위한 반복문
+	for (int i = 0; i < strlen(S); i++) {
+		for (int j = 0; j < R; j++)
+			printf("%c", S[i]);
+	}
+
+	printf("\n");
+}
+return 0;
+}
+
