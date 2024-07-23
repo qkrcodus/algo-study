4ed57619d8c5de0999e0844f2c9d74e1d0eabc67
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2024-07-23 20:31:50 +0900

    [Silver III] Title: 주유소, Time: 92 ms, Memory: 2804 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -1,32 +1,32 @@
 #include <iostream>
 #include <algorithm>
-
+#include <vector>
 using namespace std;
 int main() {
 	int N;
 	cin >> N;
 	//도시별 기름 값
-	int cost[100000];
+	vector<int> cost(N);
 	//도시 사이 거리 
-	int distance[1000000];
-	//저장할 곳
-	int dp[1000000];
-	distance[0] = 0;
-	cost[0] = 0;
-	for (int i = 0; i < N ; i++) {
-		cin >> distance[i+1];
+	vector<int> distance(N - 1);
+
+	for (int i = 0; i < N - 1; i++) {
+		cin >> distance[i];
 	}
+
 	for (int i = 0; i < N; i++) {
-		cin >> cost[i+1];
+		cin >> cost[i];
 	}
-	dp[0] = 0;
-	dp[1] = 0;
-	dp[2] = cost[1] * distance[1];
 
-	for (int i = 3; i < N + 1; i++) {
+	long long min_cost = cost[0];
+	long long total_cost = 0;
 
-		int add = min(cost[i - 2], cost[i - 1]);
-		dp[i] = add * distance[i - 1] + dp[i - 1];
+	for (int i = 0; i < N - 1; i++) {
+		if (cost[i] < min_cost) {
+			min_cost = cost[i];
+		}
+		total_cost += min_cost * distance[i];
 	}
-	cout << dp[N];
+
+	cout << total_cost;
 }
